b8a32f54ccb2750badf5ec214a18d8e39b7e8945
Phil <phil.github@gmail.com>
2024-04-03 12:55:59 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(word) {
+  const newArray = cats.push(word);
+  return newArray;
+}
+
+function destructivelyPrependCat(word) {
+  return cats.unshift(word);
+}
+
+function destructivelyRemoveLastCat() {
+  return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  return cats.shift();
+}
+
+function appendCat(word) {
+  const append = [...cats];
+  append.push(word);
+  return append;
+}
+
+function prependCat(name) {
+  const prepend = [...cats];
+  prepend.unshift(name);
+  return prepend;
+}
+
+function removeLastCat() {
+  const remove = [...cats];
+  remove.pop();
+  return remove;
+}
+
+function removeFirstCat() {
+  const first = [...cats];
+  first.shift();
+  return first;
+}
